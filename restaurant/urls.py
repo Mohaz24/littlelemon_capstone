d6a74334adf473674e14b3564983cd60c331aaec
Mohaz <107796482+Mohaz24@users.noreply.github.com>
2024-09-15 21:44:24 +1000

    Create booking models

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MenuView, SingleMenuView
+from .views import MenuView, SingleMenuView, BookingView
 
 urlpatterns = [
   path('menu/', MenuView.as_view()),
-  path('menu/<int:pk>', SingleMenuView.as_view())
+  path('menu/<int:pk>', SingleMenuView.as_view()),
+  path('booking/', BookingView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import permission_classes
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Menu, Booking
@@ -14,7 +16,7 @@ class MenuView(APIView):
  
     def post(self, request):
         serialize_items = MenuSerializer(data=request.data)
-        if serialize_items.is_valid():
+        if serialize_items.is_valid(raise_exception=True):
             serialize_items.save()
         return Response(serialize_items.data, status.HTTP_201_CREATED)
     
@@ -23,3 +25,15 @@ class SingleMenuView(APIView):
         items = get_object_or_404(Menu, pk=pk)
         serialize_items = MenuView(items)
         return Response(serialize_items.data, status.HTTP_200_OK)
+    
+class BookingView(APIView):
+    def get(self, request):
+        items = Booking.objects.all()
+        serialize_items = BookingSerializer(items, many=True)
+        return Response(serialize_items.data, status.HTTP_200_OK)
+   
+    def post(self, request):
+         serialize_items = BookingSerializer(data=request.data)
+         if serialize_items.is_valid(raise_exception=True):
+             serialize_items.save()
+         return Response(serialize_items.data, status.HTTP_201_CREATED)
